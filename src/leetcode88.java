e555b9cf93b7f8c4acc95f3e1c3c7a9357d0e210
Aman gupta <113242405+Amangupta7011@users.noreply.github.com>
2024-09-11 04:43:21 +0530

    done

---
+++ src/leetcode88.java
@@ -0,0 +1,14 @@
+public class leetcode88 {
+   // https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+   public static void main(String[] args) {
+       // Solution {
+       //    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       //        for (int j = 0, i = m; j < n; j++) {
+       //            nums1[i] = nums2[j];
+       //            i++;
+       //        }
+       //        Arrays.sort(nums1);
+       //    }
+       //    }
+   }
+}
