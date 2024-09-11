b64416899c1eb66aa22b4d1e68d973b03536e9b6
Aman gupta <113242405+Amangupta7011@users.noreply.github.com>
2024-09-11 12:21:13 +0530

    done

---
+++ src/leetcode28.java
@@ -0,0 +1,10 @@
+public class leetcode28 {
+   // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+
+    //class Solution {
+    //    public int strStr(String haystack, String needle) {
+    //       return haystack.indexOf(needle);
+    //    }
+    //}
+}
