6b395d9fb8c5c98393ad91bac8ec4662c1670e35
Aman gupta <113242405+Amangupta7011@users.noreply.github.com>
2024-09-11 12:52:53 +0530

    done

---
+++ src/leetcode26.java
@@ -0,0 +1,15 @@
+public class leetcode26 {
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+    //class Solution {
+    //    public int removeDuplicates(int[] nums) {
+    //        int j=0;
+    //        for(int i=0;i<nums.length;i++){
+    //if(nums[j]!=nums[i]){
+    //    nums[++j]=nums[i];
+    //}
+    //        }
+    //        return j+1;
+    //    }
+    //}
+}
